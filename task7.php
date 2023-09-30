4d76e7d63fbbefa29cfdfb598a7af5a2f60712a7
rowankhaled2902 <144149089+rowankhaled2902@users.noreply.github.com>
2023-09-30 10:12:38 +0300

    Update task7.php

--- task7.php
+++ task7.php
@@ -1,79 +1,81 @@
 <?php
 //1-create database using mysqli:
 
-// $connection=new mysqli ('localhost','root','');
-// if ($connection->connect_error) {
-//     die("failed");
-// }
-// echo "connected successfully";
+ $connection=new mysqli ('localhost','root','');
+ if ($connection->connect_error) {
+     die("failed");
+ }
+ echo "connected successfully";
 
-// $connection->query("create database Employee");
+ $connection->query("create database Employee");
 
-
+---------------------------------------------------
+    
 //2-create table using mysqli:
 
-// $connection=new mysqli ('localhost','root','','Employee');
-// if ($connection->connect_error) {
-//     die("failed");
-// }
-// echo "connected successfully";
+ $connection=new mysqli ('localhost','root','','Employee');
+ if ($connection->connect_error) {
+     die("failed");
+ }
+ echo "connected successfully";
 
-// $connection->query("create table Employee(id int(20)AUTO_INCREMENT primary key,
-// fname text(20),
-// lname text(20),
-// email text(20),
-// address text(20),
-// salary int(20),
-// age int(20))");
+ $connection->query("create table Employee(id int(20)AUTO_INCREMENT primary key,
+ fname text(20),
+ lname text(20),
+ email text(20),
+ address text(20),
+ salary int(20),
+ age int(20))");
 
+---------------------------------------------
 
 //3- insert 3 rows in Employees table using mysqli:
 
-// $connection->query("insert into Employee(fname,lname,email,address,salary,age)
-// values
-// ('mohammed','khaled','mkhaled@gmail.com','nasr city',3500,23),
-// ('asmaa','galal','asmaag12@gmail.com','maddi',7000,25),
-// ('nouran','nasr','nonmnase272@gmail.com','madinty',12500,30)
-// ");
+ $connection->query("insert into Employee(fname,lname,email,address,salary,age)
+ values
+ ('mohammed','khaled','mkhaled@gmail.com','nasr city',3500,23),
+ ('asmaa','galal','asmaag12@gmail.com','maddi',7000,25),
+ ('nouran','nasr','nonmnase272@gmail.com','madinty',12500,30)
+ ");
 
-// $connection->close();
+ $connection->close();
 
-
+--------------------------------------------------
 //1-create database using pdo
 
-// try{
-// $conn= new PDO ("mysql:host=localhost","root","");
-// $conn->query("CREATE DATABASE employee_pdo");
+ try{
+ $conn= new PDO ("mysql:host=localhost","root","");
+ $conn->query("CREATE DATABASE employee_pdo");
 
-// echo "Database created successfully<br>";
-// }catch(PDOEXCEPTION $error) {
-//     echo $error ;
-// }
-// $conn=null;
+ echo "Database created successfully<br>";
+ }catch(PDOEXCEPTION $error) {
+     echo $error ;
+ }
+ $conn=null;
 
-
+--------------------------------------------------
 // 2-create table using pdo:
-// try{
-//     $conn= new PDO ("mysql:host=localhost;dbname=employee_pdo","root","");
-//     $conn->query("CREATE table employees (id int(20)AUTO_INCREMENT primary key,
-//      fname text(20),
-//      lname text(20),
-//     email text(20),
-//      address text(20),
-//      salary int(20),
-//      age int(20))");
+ try{
+    $conn= new PDO ("mysql:host=localhost;dbname=employee_pdo","root","");
+     $conn->query("CREATE table employees (id int(20)AUTO_INCREMENT primary key,
+      fname text(20),
+      lname text(20),
+     email text(20),
+      address text(20),
+      salary int(20),
+      age int(20))");
     
-//     echo "Table created successfully<br>";
-//     }catch(PDOEXCEPTION $error) {
-//         echo $error ;
-//     }
-//     $conn=null;
+     echo "Table created successfully<br>";
+     }catch(PDOEXCEPTION $error) {
+        echo $error ;
+     }
+     $conn=null;
 
 
-
+----------------------------------------------
 //3- insert 3 rows in Employees table using pdo and html form
  ?>
-<!-- <!DOCTYPE html>
+ <!DOCTYPE html>
  <html lang="en">
 
  <head>
@@ -95,35 +97,36 @@
     </form>
  </body>
 
-</html> -->
+</html> 
 <?php
-// try{
-//     $conn= new pdo ("mysql:host=localhost;dbname=employee_pdo","root","");
-// echo "connected successfully";
+ try{
+     $conn= new pdo ("mysql:host=localhost;dbname=employee_pdo","root","");
+ echo "connected successfully";
 
-// var_dump($_POST);
+ var_dump($_POST);
 
-// $fname=$_POST['fname'];
-// $lname=$_POST ['lname'];
-// $email=$_POST ['email'];
-// $address=$_POST['address'];
-// $salary=$_POST['salary'];
-// $age=$_POST['age'];
+ $fname=$_POST['fname'];
+ $lname=$_POST ['lname'];
+ $email=$_POST ['email'];
+ $address=$_POST['address'];
+ $salary=$_POST['salary'];
+ $age=$_POST['age'];
 
-// $conn->query("insert into employees(fname,lname,email,address,salary,age)
-// values ('$fname','$lname','$email','$address','$salary','$age')");
-// echo"inserted";
-// } catch(PDOEXCEPTION $error){
-//     echo $error ;
+ $conn->query("insert into employees(fname,lname,email,address,salary,age)
+ values ('$fname','$lname','$email','$address','$salary','$age')");
+ echo"inserted";
+ } catch(PDOEXCEPTION $error){
+     echo $error ;
 
-// }
-
-
-
-// $conn=null;
+ }
 
 
+
+$conn=null;
+
+------------------------------------------------
 //7- list all Employees into html table using pdo connection
+//8- show one Employee when you click on show html button
 ?>
 
 
